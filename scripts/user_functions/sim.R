4b9b8b8f6250b21f726ecd9d330f4bdd4b23230b
Joshua Entrop <entjos@users.noreply.github.com>
2023-11-02 13:34:56 +0100

    Implemented new simreccomp_adapted function in sim call

--- scripts/user_functions/sim.R
+++ scripts/user_functions/sim.R
@@ -22,28 +22,30 @@
 sim <- function(n, par_rec, par_comp) {
   
   # Load modules
-  box::use(simrec[simreccomp],
-           usr = scripts/user_functions)
+  box::use(usr = scripts/user_functions)
   
   # Run simulation
-  temp <- simreccomp(n,
-                     fu.min = 0,
-                     fu.max = 10,
-                     cens.prob = 0,
-                     dist.x = "binomial",
-                     par.x = 0.5,
-                     beta.xr = 1.2,
-                     beta.xc = 0.4,
-                     dist.zr = "gamma",
-                     par.zr = 1,
-                     dist.zc = "gamma",
-                     par.zc = 0,
-                     dist.rec = "weibull",
-                     par.rec = c(par_rec$scale, par_rec$shape, par$scale_inc),
-                     dist.comp = "weibull",
-                     par.comp = c(par_comp$scale, par_comp$shape),
-                     pfree = 1,
-                     dfree = 0.001)
+  temp <- usr$simreccomp_adapted(n,
+                                 fu.min = 0,
+                                 fu.max = 10,
+                                 cens.prob = 0,
+                                 dist.x = "binomial",
+                                 par.x = 0.5,
+                                 beta.xr = 1.2,
+                                 beta.xc = 0.4,
+                                 dist.zr = "gamma",
+                                 par.zr = 1,
+                                 dist.zc = "gamma",
+                                 par.zc = 0,
+                                 dist.rec = "weibull",
+                                 par.rec = c(par_rec$scale, 
+                                             par_rec$shape, 
+                                             par_rec$scale_inc),
+                                 dist.comp = "weibull",
+                                 par.comp = c(par_comp$scale, 
+                                              par_comp$shape),
+                                 pfree = 1,
+                                 dfree = 0.001)
   
   # Transform simulated dataset into stacked format
   out <- usr$stack_simreccomp(temp)
