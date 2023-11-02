4b1105b3bfd6b4410ce9419fd4c23efd77c8aeb9
Joshua Entrop <entjos@users.noreply.github.com>
2023-11-02 13:35:40 +0100

    replaced old benchmarking function with new mean_no function from the JointFPM package

--- scripts/simulations/2_get_benchmark.R
+++ scripts/simulations/2_get_benchmark.R
@@ -16,36 +16,36 @@ box::use(par = parallel)
 
 # 2. Set Up Parallelisation ----------------------------------------------------
 
-cl <- par$makeCluster(4, type = "SOCK")
+cl <- par$makeCluster(10, type = "SOCK")
 
 # 3. Run Simulations -----------------------------------------------------------
-par$clusterApply(cl, seq_along(dir("./data/sim_data")), function(i) {
+par$clusterApply(cl, 1:10, function(i) {
   
   # Load packages
-  box::use(usr = scripts/user_functions,
-           dt  = data.table,
-           survival[Surv])
+  box::use(dt  = data.table,
+           survival[Surv],
+           JointFPM[mean_no])
   
   # Load data
   sim_data <- dt$fread(paste0("./data/sim_data/sim", i, ".csv"))
   
   # Obtain benchmark estimates
-  temp <- usr$benchmarking(sim_data,
-                           by_var = "x",
-                           arg_mean_no = 
-                             list(formula      = Surv(start, stop, status) ~ 1,
-                                  re_indicator = "re",
-                                  ce_indicator = "ce",
-                                  data         = sim_data,
-                                  re_control   = list(timefix = FALSE)))
+  temp <- mean_no(formula      = Surv(start, stop, status) ~ x,
+                  re_indicator = "re",
+                  ce_indicator = "ce",
+                  data         = sim_data,
+                  re_control   = list(timefix = FALSE))
+  
+  temp[, x := gsub("x=", "", strata)]
+  temp[, strata := NULL]
   
   # Export dataset
   dt$fwrite(temp, paste0("./data/sim_benchmark/sim", i, ".csv"))
   
   # Success
   return(1)
-
-  })
+  
+})
 
 # 4. Clean up ------------------------------------------------------------------
 
