16f923d1169d849017c4877c6dfa4982d2df1c4d
Joshua Entrop <entjos@users.noreply.github.com>
2024-12-10 12:07:20 +0100

    Add master script

---
+++ master_script.R
@@ -0,0 +1,52 @@
+################################################################################
+# Project: Parametic Estimation of The Mean Number of Events
+# 
+# Title: Master Script
+# 
+# Author: Joshua Entrop
+#
+# Notes: This master script can be used in case one cannot use the MAKE file.
+#        Note that the MAKE file is the recommended way of reproducing the
+#        analysis.
+#
+#        The simulation parameters can be changed in the options included in the
+#        .Rprofile file. The options that can be changed include:
+#           - the number of Monte Carolo iterations
+#           - the size of each randome draw, and 
+#           -the number of computational cluster used.
+# 
+################################################################################
+
+
+# PART 1 SIMULATION STUDY ------------------------------------------------------
+
+#   1.1 Create Simulation Data ================================================= 
+source("scripts/simulations/1_simulate_data.R")
+
+#   1.2 Get Benchmark Estiamtes ================================================
+source("scripts/simulations/2_get_benchmark.R")
+
+#   1.3 Main Analysis ==========================================================
+source("scripts/simulations/3_1_test_fpm_model_fit_mean_no.R")
+source("scripts/simulations/3_2_test_fpm_model_fit_diff.R")
+
+#   1.4 Supplementary Analyses =================================================
+source("scripts/simulations/3_3_test_fpm_model_fit_cumhaz.R")
+source("scripts/simulations/3_4_test_fpm_model_fit_cont_x.R")
+source("scripts/simulations/3_5_test_ghosh_lin.R")
+
+#   1.5 Report Model Performance Measures ======================================
+
+source("scripts/simulations/4_get_model_fit_estimates.R")
+source("scripts/simulations/5_bias_plot.R")
+source("scripts/simulations/6_plots_of_benchmark_estimates.R")
+
+# PART 2 EXAMPLE ---------------------------------------------------------------
+
+source("scripts/example/1_analysis.R")
+source("scripts/example/2_main_figures.R")
+source("scripts/example/3_1_appendix_figures_S1_S2.R")
+source("scripts/example/3_2_appendix_figure_S3.R")
+
+# //////////////////////////////////////////////////////////////////////////////
+# END OF R-SCRIPT
