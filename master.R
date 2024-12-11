2f553aea65a374d0d30f1b7d7d2ac54ffe6c3cda
Joshua Entrop <entjos@users.noreply.github.com>
2024-12-11 12:26:04 +0100

    Improve comments in master file

--- master.R
+++ master.R
@@ -11,9 +11,9 @@
 #
 #        The simulation parameters can be changed in the options included in the
 #        .Rprofile file. The options that can be changed include:
-#           - the number of Monte Carolo iterations
-#           - the size of each randome draw, and 
-#           -the number of computational cluster used.
+#           - the number of Monte Carlo iterations
+#           - the size of each random draw, and 
+#           - the number of computational cluster used.
 # 
 ################################################################################
 
@@ -36,17 +36,16 @@ source("scripts/simulations/3_4_test_fpm_model_fit_cont_x.R")
 source("scripts/simulations/3_5_test_ghosh_lin.R")
 
 #   1.5 Report Model Performance Measures ======================================
-
-source("scripts/simulations/4_get_model_fit_estimates.R")
-source("scripts/simulations/5_bias_plot.R")
-source("scripts/simulations/6_plots_of_benchmark_estimates.R")
+source("scripts/simulations/4_get_model_fit_estimates.R")      # Table 3
+source("scripts/simulations/5_bias_plot.R")                    # Figure 3
+source("scripts/simulations/6_plots_of_benchmark_estimates.R") # Figure 2
 
 # PART 2 EXAMPLE ---------------------------------------------------------------
 
 source("scripts/example/1_analysis.R")
-source("scripts/example/2_main_figures.R")
-source("scripts/example/3_1_appendix_figures_S1_S2.R")
-source("scripts/example/3_2_appendix_figure_S3.R")
+source("scripts/example/2_main_figures.R")                     # Figure 4
+source("scripts/example/3_1_appendix_figures_S1_S2.R")         # Figures S1, S2
+source("scripts/example/3_2_appendix_figure_S3.R")             # Figure S3
 
 # //////////////////////////////////////////////////////////////////////////////
 # END OF R-SCRIPT
